e0c3ab902b68e6bf281ac92031cd21aac899c0ab
Poyu Chen <p820316@gmail.com>
2017-05-16 14:04:08 -0700

    [model] Finished note model

--- controllers/notesController.js
+++ controllers/notesController.js
@@ -1,14 +1,63 @@
+const Note = require('../models/note')
+
 module.exports = {
   createNote: (req, res) => {
-    res.send('createNote')
+    console.log('createNote, req.body: ', req.body)
+    Note.create(req.body)
+    .then(note => res.json(note))
+    .catch(err => res.json(err))
   },
+
   readNote: (req, res) => {
-    res.send('readNote')
+    console.log('readNote, req.params.uuid: ', req.params.uuid)
+    Note.findOne({uuid: req.params.uuid})
+    .then((note) => {
+      if (!note) return Promise.reject(new Error('Record not found!'))
+      const {md_content, created_at, last_modifid} = note
+      res.json({md_content, created_at, last_modifid})
+    })
+    .catch(err => res.json({hasError: true, message: err.message}))
   },
+
   updateNote: (req, res) => {
-    res.send('updateNote')
+    const { edit_key, md_content } = req.body
+    const { uuid } = req.params
+    const now = Date.now()
+    console.log('updateNote, req.params.uuid: ', uuid)
+    Note.findOne({ uuid })
+    .then(note => {
+      if (!note || edit_key !== note.edit_key) {
+        return Promise.reject(new Error('Record not found or edit_key wrong!'))
+      }
+
+      const newNote = Object.assign(note, {
+        md_content: md_content,
+        last_modifid: now
+      })
+      return Note.findOneAndUpdate({ uuid }, newNote)
+    }).then(note => {
+      res.json({hasError: false, last_modifid: (new Date(now)).toISOString()})
+    }).catch(err => {
+      res.json({hasError: true, message: err.message})
+    })
   },
+
   deleteNote: (req, res) => {
-    res.send('deleteNote')
+    const { edit_key } = req.body
+    const { uuid } = req.params
+
+    console.log('deleteNote, req.params.uuid: ', uuid)
+    Note.findOne({ uuid })
+    .then(note => {
+      if (!note || edit_key !== note.edit_key) {
+        return Promise.reject(new Error('Record not found or edit_key wrong!'))
+      }
+
+      return Note.findOneAndRemove({ uuid })
+    }).then(note => {
+      res.json({hasError: false})
+    }).catch(err => {
+      res.json({hasError: true, message: err.message})
+    })
   }
 }
